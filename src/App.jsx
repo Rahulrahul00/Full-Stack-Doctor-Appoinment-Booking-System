dc8a9232bae1b3bf4e09d2c83a2a4d7427eaa16b
Rahul c shaji <rahulcshaji007@gmail.com>
2025-01-09 16:12:17 +0530

    Started a new project called 'DocTym Doctor Booking Appointment System' and installed all dependencies

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-green-600 text-center font-bold text-[20rem]'>
+      rahul
+    </div>
+  )
+}
+
+export default App
