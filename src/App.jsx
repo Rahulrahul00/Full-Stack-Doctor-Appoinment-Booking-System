f7898c40233e602fa9af6f72646c6d7b4dbc0f8a
Rahul c shaji <rahulcshaji007@gmail.com>
2025-01-09 18:43:54 +0530

    Set up all route pages, including Home, Doctors, About, Contact, My Appointment, My Profile, Login, and Appointment.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Doctors from './pages/Doctors'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import MyAppoinment from './pages/MyAppoinment'
+import MyProfile from './pages/MyProfile'
+import Appoinments from './pages/Appoinments'
 
 const App = () => {
   return (
-    <div className='text-green-600 text-center font-bold text-[20rem]'>
-      rahul
+    <div className='mx-4 sm:mx-[10%]'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/doctors' element={<Doctors/>} />
+        <Route path='/doctors/:speciality' element={<Doctors/>}/>
+        <Route path='/login' element={<Login/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route patth='/my-profile' element={<MyProfile/>} />
+        <Route path='/my-appoinments' element={<MyAppoinment/>} />
+        <Route path='/appoinments/:docId' element={<Appoinments/>} />
+
+        
+       
+
+      </Routes>
+      
     </div>
   )
 }
